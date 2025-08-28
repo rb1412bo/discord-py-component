631bc4b30a2ce2e933a27e7a68f7a831f5fc9274
rb1412bo <rb.larpg@gmail.com>
2025-08-28 13:52:03 +0200

    Update init_.py

--- discordcomponents/init_.py
+++ discordcomponents/init_.py
@@ -1,10 +1,29 @@
 # discord_components/__init__.py
 
 from .http_patch import setup_http_patch
-from .components import __all__ as _components
+from .components import (
+    __all__ as _COMP_NAMES,
+    ActionRow,
+    Button,
+    StringSelect,
+    TextInput,
+    UserSelect,
+    RoleSelect,
+    MentionableSelect,
+    ChannelSelect,
+    DatePicker,
+    TextDisplay,
+    Separator,
+    Container,
+    Section,
+    Thumbnail,
+    MediaGallery,
+    FileComponent,
+    Label,
+)
 
 def setup(bot):
     # Monkey‐patch the internal HTTPClient.create_message
     setup_http_patch()
-    # Add all component classes as bot.ComponentsV2
-    bot.ComponentsV2 = {name: globals()[name] for name in _components}
+    # Expose all component classes under bot.ComponentsV2
+    bot.ComponentsV2 = {name: globals()[name] for name in _COMP_NAMES}
